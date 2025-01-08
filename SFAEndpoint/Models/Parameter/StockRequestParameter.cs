d6d5b2d1397eff87e9010a336e6a81b1e6626560
kelvinrayner <kelvinnainggolan59@gmail.com>
2025-01-08 10:10:53 +0700

    Update date format

--- SFAEndpoint/Models/Parameter/StockRequestParameter.cs
+++ SFAEndpoint/Models/Parameter/StockRequestParameter.cs
@@ -5,7 +5,7 @@
         public int salesCode { get; set; }
         public string salesName { get; set; }
         public string skaRefrenceNumber { get; set; }
-        public string requestDate { get; set; }
+        public DateOnly requestDate { get; set; }
         public List<StockRequestDetailParameter> detail { get; set; }
     }
 }
